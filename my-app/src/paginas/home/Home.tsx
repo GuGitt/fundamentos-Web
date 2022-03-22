d20a41bfb03d96d3acd245688d43d685b100db12
Gustavo <gugul5@hotmail.com>
2022-03-22 15:53:07 -0300

    Front tier

---
+++ my-app/src/paginas/home/Home.tsx
@@ -0,0 +1,13 @@
+import react from 'react';
+import './home.css';
+
+function Home(){
+    return(
+        <>
+        <h1 className="Titulo">Home</h1>
+        <img src="https://i.pinimg.com/736x/36/b0/c3/36b0c38c48ad0cd9cf17a1479c63e1a9.jpg" alt="Gatinho triste" className="img"  />
+        </>
+    );
+}
+
+export default Home;
